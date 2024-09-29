122e691b533764176658412c0be1240dc14a9ca6
mrkihiki <mrkihiki@yandex.ru>
2024-09-29 19:40:19 +0700

    F

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,34 +1,45 @@
 package org.example;
 
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-//        System.out.printf("Hello and welcome!");
+        //System.out.printf("Hello and welcome!");
+        System.out.println("Task 1");
         System.out.println(triangle(7,4,9));
-        System.out.println(tribonacci(7));
+        System.out.println("Task 2");
         System.out.println(distance(0,0,1,1));
+        System.out.println("Task 3");
         System.out.println(powerLoop(2,-4));
+        System.out.println("Task 4");
+        System.out.println(power(2,4));
+        System.out.println("Task 5");
+        System.out.println(tribonacci(7));
     }
+//    public static boolean triangle(float a, float b, float c){
+//        return triangle(a,b,c,0);
+//    }
+//    public static boolean triangle(float a, float b, float c,int z){
+//        if(a<=0 || b<=0 || c<=0){
+//            return false;
+//        }
+//        z++;
+//        if (a+b>c){
+//            if (3 == z){
+//                return true;
+//            }
+//            return triangle(b,c,a,z);
+//        }
+//        else {
+//            return false;
+//        }
+//    }
     public static boolean triangle(float a, float b, float c){
-        return triangle(a,b,c,0);
-    }
-    public static boolean triangle(float a, float b, float c,int z){
-        if(a<=0 || b<=0 || c<=0){
-            return false;
-        }
-        z++;
-        if (a+b>c){
-            if (3 == z){
-                return true;
-            }
-            return triangle(b,c,a,z);
-        }
-        else {
-            return false;
-        }
+        if(a<=0 || b<=0 || c<=0){return false;}
+        return a + b > c && a + c > b && b + c > a;
     }
     public static double distance(float x1, float y1, float x2, float y2){
         return Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2));
@@ -45,10 +56,11 @@ public class Main {
         if(n<0) return null;
         return a*power(a,n-1);
     }
-    public static int tribonacci (int n){
+    public static Integer tribonacci (int n){
         if (n==0){return 0;}
         if (n==1){return 0;}
         if (n==2){return 1;}
-        return n= tribonacci(n-1)+ tribonacci(n-2)+ tribonacci(n-3);
+        if (n<0){return null;}
+        return tribonacci(n-1)+ tribonacci(n-2)+ tribonacci(n-3);
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -15,17 +15,25 @@ class MainTest {
 
     @Test
     void distance() {
+        assertEquals(1,Main.distance(0,0,1,0));
+        assertEquals(1.4142135623730951,Main.distance(0,0,1,1));
     }
 
     @Test
     void powerLoop() {
+        assertEquals(0.0625,Main.powerLoop(2,-4));
+        assertEquals(16,Main.powerLoop(2,4));
     }
 
     @Test
     void power() {
+        assertEquals(16,Main.powerLoop(2,4));
+        assertNull(Main.power(2,-4));
     }
 
     @Test
     void tribonacci() {
+        assertEquals(13,Main.tribonacci(7));
+        assertNull(Main.tribonacci(-1));
     }
 }
